27cf4f632518448e7b06d5e2cf84e40125bb3357
David Maria <eldavid_46@hotmail.com>
2020-09-26 11:11:36 -0400

    Agregando el manejador de excepciones

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,10 @@ namespace prestamos
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"Paso algo feo :( {e.Exception.Message}", "");
+            e.Handled = true;
+        }
     }
 }
